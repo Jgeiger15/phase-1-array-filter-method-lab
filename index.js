17014c86d6f09506a9083f1fbc33cf940127818b
Jakob Geiger <jakobgeiger15@gmail.com>
2022-03-04 21:47:58 -0500

    commit tree b24fe2336839cb2d068812939f1e392b05d0bb6a

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+//const { stringMatching } = require("expect");
+
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+//const result = drivers.filter(element => element === string)
+
+function findMatching(drivers, string) {
+  let result = drivers.filter(element => element.toLowerCase() === string.toLowerCase())
+  return result;  
+    
+}
+
+function fuzzyMatch (drivers, string) {
+    let result = drivers.filter(element => element.startsWith(string))
+    return result;
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(drivers, string) {
+    const result = drivers.filter(element => element.name === string)
+    return result;
+}
